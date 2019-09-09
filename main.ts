55d817edc0db97bac38b403b52cb3e63062a9899
genix9876 <54391269+genix9876@users.noreply.github.com>
2019-09-09 17:46:32 +0530

    Add files via upload

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ basic.showLeds(`
 
 //% color=#008000 icon="\uf556"
 namespace test{
-    //% blockId=print weight=70 blockGap=30
+    //% blockId=printName weight=70 blockGap=30
     //% block="Enter a string to print %name"
     export function showname(name: string) {
         basic.showString(name);
